6c4b67ccfb5c070a91d8f81b73f12f358361ba56
gurram_reddy_projectexplorer <154806581+gurramnani@users.noreply.github.com>
2025-09-10 15:42:52 +0530

    Update FirstNonRepeatingChar.java

--- FirstNonRepeatingChar.java
+++ FirstNonRepeatingChar.java
@@ -12,8 +12,9 @@ public class FirstNonRepeatingChar {
         System.out.println();
         String s = "aabbcc"; //aabbccddeffg aabbcc programming
         s.chars().mapToObj(c->(char)c).
-                collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting())).
+                collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting())).
                 entrySet().stream().filter(e->e.getValue()==1).findFirst().
                 ifPresentOrElse((e)->System.out.println(e),()-> System.out.println("Non Repeating chars are not present"));
     }
 }
+
