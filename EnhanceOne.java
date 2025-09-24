2739f97f6960be2382b2bf9761715d775a294b94
gurram_reddy_projectexplorer <154806581+gurramnani@users.noreply.github.com>
2025-09-24 22:46:16 +0530

    Practice

--- EnhanceOne.java
+++ EnhanceOne.java
@@ -2,9 +2,7 @@ package com.demo.exam;
 
 import com.demo.model.java8.Staff;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class EnhanceOne {
@@ -24,5 +22,21 @@ public class EnhanceOne {
 
         System.out.println("Find distinct first letters of all staff names. (map + distinct)");
         staffList.stream().map(Staff::getName).map(s->s.substring(0,1)).distinct().forEach(System.out::println);
+
+        System.out.println("Sort staff by name in reverse order and collect names into a list.");
+        List<String> reverseOrderNames =  staffList.stream().map(Staff::getName).sorted(Comparator.comparing(String::valueOf).reversed()).collect(Collectors.toList());
+        System.out.println(reverseOrderNames);
+
+        System.out.println("Find the staff with the longest name");
+        staffList.stream().max(Comparator.comparing(s->s.getName().length())).ifPresent(System.out::println);
+        staffList.stream().map(Staff::getName).forEach(s-> System.out.println(s.length()+":"+s));
+
+        System.out.println("Return optional staff whose email ends with @mail.com ");
+        Optional<Staff> endsWithEmail = staffList.stream().filter(s->s.getEmail().endsWith("@mail.com")).findFirst();
+        System.out.println(endsWithEmail);
+
+        System.out.println("Find department-wise highest salary staff");
+        Map<String,Optional<Staff>> highestSalaryByDepartment = staffList.stream().collect(Collectors.groupingBy(Staff::getDepartment,Collectors.maxBy(Comparator.comparing(Staff::getSalary))));
+        System.out.println(highestSalaryByDepartment);
     }
 }
